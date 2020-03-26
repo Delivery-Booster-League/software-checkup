e0a9195814af8aa9755333d8eea22f6d9c6a0820
dgeffroy <dan.geffroy@capgemini.com>
2020-03-26 17:47:04 +0100

    changing color theme

--- src/app/mindmap/mindmap.component.ts
+++ src/app/mindmap/mindmap.component.ts
@@ -56,11 +56,11 @@ export class MindmapComponent implements OnChanges, AfterViewInit {
   (d3.hierarchy(data)
     .sum(d => d.children? d.children.lenth:1)
     .sort((a, b) => b.value - a.value))
-    d3.select('svg').remove();
+    
   }
 
   private createChart(openMode:string): void {
-
+    d3.select('svg').remove();
 
     // Set the dimensions and margins of the diagram
     const element = this.chartContainer.nativeElement;
@@ -69,14 +69,12 @@ export class MindmapComponent implements OnChanges, AfterViewInit {
     const root :any = this.pack(this.data,element.offsetWidth, element.offsetHeight);
   let focus = root;
   let view;
-
-  var color = d3.interpolateCool;
+  var color = ["#64bebf","#3dacad","#008182","#006d6d","#006566"]
   var format = d3.format(",d")
   const svg = d3.select("#chart").append("svg")
       .attr("viewBox", `-${element.offsetWidth  / 2} -${element.offsetHeight / 2} ${element.offsetWidth } ${element.offsetHeight}`)
       .style("display", "block")
-      .style("margin", "0 -14px")
-      .style("background", "#FFFF")
+      .style("background", color[0])
       .style("cursor", "pointer")
       .on("click", () => zoom(root));
 console.log(root);
@@ -84,7 +82,7 @@ console.log(root);
     .selectAll("circle")
     .data(root.descendants().slice(1))
     .join("circle")
-      .attr("fill", (d:any) => d.children ? color(d.depth) : "white")
+      .attr("fill", (d:any) => color[d.depth])
       .on("mouseover", function() { d3.select(this).attr("stroke", "#000"); })
       .on("mouseout", function() { d3.select(this).attr("stroke", null); })
       .on("click", (d:any) => {        
@@ -100,15 +98,18 @@ console.log(root);
       });
 
   const label :any = svg.append("g")
-      .style("font", "15px sans-serif")
+      .style("font", "20px sans-serif")
+      .style("fill", "white")
       .attr("pointer-events", "none")
       .attr("text-anchor", "middle")
 
     .selectAll("text")
     .data(root.descendants())
     .join("text")
-      .style("fill-opacity", (d:any) => d.parent === root ? 1 : 0)
-      .style("display", (d:any) => d.parent === root ? "inline" : "none")
+      .style("font", (d:any) => d.children ? "25px sans-serif" : "10px sans-serif")
+      .style("fill",(d:any)=> d.children ?"#FDD835":"white")
+      .style("fill-opacity", d => d === focus ? 0 : 1)
+      .style("fill-opacity", (d:any) => d.parent === focus|| !d.children ? 1 : 0)
       .text((d:any) => d.data.name);
 
 
@@ -119,7 +120,20 @@ console.log(root);
 
     view = v;
 
-    label.attr("transform", (d:any) => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
+    label.attr("transform", (d:any) => {
+      if(d.children){
+        if(d.depth==1){
+          return `translate(${(d.x - v[0]) * k-d.r/2},${(d.y - v[1]) * k-d.r+25})`;
+        }
+        else{
+          return `translate(${(d.x - v[0]) * k-d.r/2},${(d.y - v[1]) * k-d.r-125})`;
+
+        }
+      }
+      else{
+        return `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`;
+      }
+    });
     node.attr("transform", (d:any) => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
     node.attr("r", (d:any) => d.r * k);
   }
@@ -137,11 +151,10 @@ console.log(root);
         });
 
     label
-      .filter(function(d) { return d.parent === focus || this.style.display === "inline"; })
       .transition(transition)
-        .style("fill-opacity", d => d.parent === focus ? 1 : 0)
-        .on("start", function(d) { if (d.parent === focus) this.style.display = "inline"; })
-        .on("end", function(d) { if (d.parent !== focus) this.style.display = "none"; });
+        .style("fill-opacity", d => d === focus && d.children ? 0 : 1)
+        .on("start", function(d) { if (d.parent === focus  ) this.style.display = "inline"; })
+        .on("end", function(d) { if (d.parent !== focus && d.children) this.style.display = "none"; });
   }
   }
 }
